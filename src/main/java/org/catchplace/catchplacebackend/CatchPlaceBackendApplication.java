988748658cd7aa069b36bee8673c9fc32af65de6
minnnisu <mine52100@naver.com>
2025-10-07 15:35:33 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/org/catchplace/catchplacebackend/CatchPlaceBackendApplication.java
@@ -0,0 +1,13 @@
+package org.catchplace.catchplacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchPlaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchPlaceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/catchplace/catchplacebackend/CatchPlaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.catchplace.catchplacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchPlaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
